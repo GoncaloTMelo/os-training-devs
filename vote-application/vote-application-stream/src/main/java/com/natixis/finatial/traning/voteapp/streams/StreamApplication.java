a5a8cc714146451cf68261e2b70103092c241829
Gonçalo Melo <goncalo.melo@natixis.com>
2022-06-22 16:51:49 +0100

    added parent to the backend pom and changes build configs for stream

--- vote-application/vote-application-stream/src/main/java/com/natixis/finatial/traning/voteapp/streams/StreamApplication.java
+++ vote-application/vote-application-stream/src/main/java/com/natixis/finatial/traning/voteapp/streams/StreamApplication.java
@@ -1,4 +1,4 @@
-package com.natixis.finatial.traning.voteapp.streams;
+package com.natixis.finatial.traning.voteapp.streams.;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
